3647365ebc28793082df6ad2526284e53e31aac2
Peter <pkabai@csumb.edu>
2018-11-27 19:57:13 -0800

    changes id

--- src/main/java/spring/UsersController.java
+++ src/main/java/spring/UsersController.java
@@ -11,7 +11,7 @@ public class UsersController {
     @Autowired
     private UsersRepository userRepository;
 
-    @CrossOrigin(origins = "*")
+    @CrossOrigin(origins = "https://team12-zoo-shop.herokuapp.com/*")
     @GetMapping ("/users")
     public List<Users> getAll() {
         return userRepository.findAll();
