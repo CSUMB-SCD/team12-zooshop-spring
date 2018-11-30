61e4e64bbd7b3e32c0a20fb24d5d4bb7a1ec3732
Peter <pkabai@csumb.edu>
2018-11-29 18:39:56 -0800

    adds put for user credit

--- src/main/java/spring/UsersController.java
+++ src/main/java/spring/UsersController.java
@@ -2,14 +2,23 @@ package spring;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UsersController {
     @Autowired
     private UsersRepository userRepository;
+
+    @CrossOrigin(origins = "*")
+    @PutMapping(value = "/put/{username}/{credit}")
+    public Users setCredits(
+            @PathVariable("username") String username,
+            @PathVariable("credit") Double credit) {
+        Users users = userRepository.findByusername(username);
+        users.setCredit(credit);
+        userRepository.save(users);
+        return users;
+    }
 
     @CrossOrigin(origins = "*")
     @GetMapping ("/users")
